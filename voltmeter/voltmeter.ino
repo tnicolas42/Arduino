11aabdbdf1ae6778b0e6a3a6db7b80e30be50e0a
tnicolas42 <tnicolas@student.42.fr>
2020-02-01 16:52:08 +0100

    add voltmeter class

---
+++ voltmeter/voltmeter.ino
@@ -0,0 +1,37 @@
+namespace Volt {
+  class Voltmeter {
+    // one analog pin
+    public:
+      Voltmeter(int voltPin) : _voltPin(voltPin), _maxVoltage(25) {}
+      virtual ~Voltmeter() {}
+
+      float getVoltage() {
+        return analogRead(_voltPin) / 1023.0 * _maxVoltage;
+      }
+
+      void setMaxVoltage(float maxVoltage) {
+        _maxVoltage = maxVoltage;
+      }
+
+    private:
+      int _voltPin;
+      float _maxVoltage;
+  };
+}
+
+// Example /////////////////////////////////////////////////
+
+int voltPin = A0;
+
+float voltage = 0;
+
+Volt::Voltmeter voltmeter(voltPin);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  voltage = voltmeter.getVoltage();
+  Serial.println(voltage);
+}
